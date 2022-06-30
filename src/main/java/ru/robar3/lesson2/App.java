6962ff5965db2538185232871ea972acf8b9d516
Robar3 <terehov94@mail.ru>
2022-06-30 22:53:31 +0300

    homework 4 java 3

---
+++ src/main/java/ru/robar3/lesson2/App.java
@@ -0,0 +1,80 @@
+package ru.robar3.lesson2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        App app = new App();
+        Thread thread1 = new Thread(() -> {
+            app.printA();
+        });
+        Thread thread2 = new Thread(() -> {
+            app.printB();
+        });
+        Thread thread3 = new Thread(() -> {
+            app.printC();
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    Thread.sleep(10);
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    Thread.sleep(10);
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    Thread.sleep(10);
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
