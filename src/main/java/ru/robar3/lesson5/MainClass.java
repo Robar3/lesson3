6b58fc871acca16e6544a471d512ca613324518b
Robar3 <terehov94@mail.ru>
2022-07-03 12:27:15 +0300

    сделана домашняя работа к 5 уроку, исправлена домашняя работа к 4 уроку

---
+++ src/main/java/ru/robar3/lesson5/MainClass.java
@@ -0,0 +1,42 @@
+package ru.robar3.lesson5;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch =new CountDownLatch(CARS_COUNT);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT+1);
+        Semaphore semaphore =new Semaphore(CARS_COUNT/2);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cyclicBarrier,semaphore,countDownLatch);
+
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
